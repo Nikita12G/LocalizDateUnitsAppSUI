492e2995e809952631b290d1ea007eb239abe11d
Nikita12G <gulyi08@mail.ru>
2024-07-25 22:14:11 +0700

    Initial Commit

---
+++ LocalizDateUnitsAppSUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalizDateUnitsAppSUI
+//
+//  Created by Никита Гуляев on 25.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalizDateUnitsAppSUI/LocalizDateUnitsAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizDateUnitsAppSUIApp.swift
+//  LocalizDateUnitsAppSUI
+//
+//  Created by Никита Гуляев on 25.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct LocalizDateUnitsAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
